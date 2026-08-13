fac241cdb973bf28f9d2568e95a87d32d9fcde29
Ashton Shears <shears.ash@gmail.com>
2026-08-12 20:21:53 -0700

    Bust deployed asset cache

--- tests/static-site.test.mjs
+++ tests/static-site.test.mjs
@@ -6,8 +6,8 @@ test("serves a static GitHub Pages entry point", async () => {
   const html = await readFile(new URL("../index.html", import.meta.url), "utf8");
 
   assert.match(html, /<title>Textfaces<\/title>/);
-  assert.match(html, /<link rel="stylesheet" href="styles\.css">/);
-  assert.match(html, /<script type="module" src="src\/app\.js"><\/script>/);
+  assert.match(html, /<link rel="stylesheet" href="styles\.css\?v=[0-9a-f]+">/);
+  assert.match(html, /<script type="module" src="src\/app\.js\?v=[0-9a-f]+"><\/script>/);
   assert.match(html, /Click to copy to clipboard/);
   assert.doesNotMatch(html, /Gag Lab|Favorites|Recently copied|search/i);
   assert.doesNotMatch(html, /next|vinext|wrangler|cloudflare/i);
